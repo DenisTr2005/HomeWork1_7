da861f47ec5ffaf782b32f04f0f42bd68ed4fb4e
Денис <den_2005@list.ru>
2022-05-04 19:52:16 +0300

    HW complete

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println("Задание 1");
+        String firstName = "Ivanov", middleName = "Ivan", lastName = "Ivanovich", fullName;
+        fullName = firstName + ' ' + middleName + ' ' + lastName;
+        System.out.println("ФИО сотрудника — " + fullName + ".");
+        System.out.println("\nЗадание 2");
+        String fullNameUpperCase = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullNameUpperCase);
+        System.out.println("\nЗадание 3");
+        fullName = "Иванов Семён Семёнович";
+        String fullNameE = fullName.replace('ё', 'е');
+        System.out.println("Данные ФИО сотрудника — " + fullNameE);
     }
 }
